fd7aeb79f0b7f76237931f70a9fa2bf1e682965f
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-27 13:27:17 -0700

    Added compareTo and colors for suits

--- src/main/java/com/tlglearning/cards/model/Card.java
+++ src/main/java/com/tlglearning/cards/model/Card.java
@@ -45,7 +45,11 @@ public class Card implements Comparable<Card> {
 
   @Override
   public int compareTo(Card other) {
-    return 0;
+    int comparison = suit.compareTo(other.suit);
+    if (comparison == 0) {
+      comparison = rank.compareTo(other.rank);
+    }
+    return comparison;
   }
 
   @Override
--- src/main/java/com/tlglearning/cards/model/Suit.java
+++ src/main/java/com/tlglearning/cards/model/Suit.java
@@ -4,23 +4,32 @@ import java.util.Locale;
 
 public enum Suit { //enum is a class
 
-  CLUBS('\u2663'),// use a \ to type in unicode
-  DIAMONDS('\u2662'),
-  HEARTS('\u2661'),
-  SPADES('\u2660');
+  CLUBS('\u2663', Color.BLACK),// use a \ to type in unicode
+  DIAMONDS('\u2662', Color.RED),
+  HEARTS('\u2661', Color.RED),
+  SPADES('\u2660', Color.BLACK);
 
   private final char symbol;
+  private final Color color;
 
-  Suit(char symbol) {//constructor does not need to be private, enum constructors default private
+  Suit(char symbol, Color color) {//constructor does not need to be private, enum constructors default private
     this.symbol = symbol;
+    this.color = color;
   }
   public char symbol() {
     return symbol;
+  }
+
+  public Color color() {
+    return color;
   }
 
   @Override //@ enlists the compiler to keep us honest, is used during documentation
   public String toString() { //inheriting a method
     String name = name();
     return name.charAt(0) + name.substring(1).toLowerCase(); //taking the 1st letter, then getting the rest, making them all lowercase. + Concatenated together
+  }
+  public enum Color {
+    BLACK, RED
   }
 }
