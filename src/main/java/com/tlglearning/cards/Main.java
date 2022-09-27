3ba618dac831a4771046ca8975fdce14bf3f8568
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-27 13:30:49 -0700

    package private top level class in Main.java

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -1,8 +1,9 @@
 package com.tlglearning.cards;
 
+import com.tlglearning.cards.model.Card;
 import com.tlglearning.cards.model.Deck;
-import com.tlglearning.cards.strategy.ColorFirst;
 import java.security.SecureRandom;
+import java.util.Comparator;
 import java.util.Random;
 
 public class Main {
@@ -20,5 +21,21 @@ public class Main {
   }
 
 }
+//package private top level class in Main.java
+class ColorFirst implements Comparator<Card> {
 
+
+  @Override
+  public int compare(Card card1, Card card2) {
+    int comparison = card1.suit().color().compareTo(card2.suit().color());
+    if (comparison == 0) {
+      comparison = card1.suit().compareTo(card2.suit());
+      if (comparison == 0) {
+        comparison = - card1.rank().compareTo(card2.rank());
+      }
+
+    }
+    return comparison;
+  }
+}
 
