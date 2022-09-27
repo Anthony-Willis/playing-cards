acfadf5d628a39ee4fcaa529f2d260f1f4ab72af
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-27 14:22:19 -0700

    Inline anonymous class in main method

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -9,15 +9,8 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
+     Comparator<Card> comparator = new Comparator<>() {
 
-    Deck deck = new Deck();
-    System.out.println(deck);
-    Random rng = new SecureRandom();
-    deck.shuffle(rng);
-    System.out.println(deck);
-    deck.sort();
-    System.out.println(deck);
-    deck.sort(new Comparator<>() {
       @Override
       public int compare(Card card1, Card card2) {
         int comparison = card1.suit().color().compareTo(card2.suit().color());
@@ -26,11 +19,22 @@ public class Main {
           if (comparison == 0) {
             comparison = - card1.rank().compareTo(card2.rank());
           }
+
         }
         return comparison;
       }
-    });
+    };
+
+    Deck deck = new Deck();
     System.out.println(deck);
+    Random rng = new SecureRandom();
+    deck.shuffle(rng);
+    System.out.println(deck);
+    deck.sort();
+    System.out.println(deck);
+    deck.sort(comparator);
+    System.out.println(deck);
+
   }
 
 }
