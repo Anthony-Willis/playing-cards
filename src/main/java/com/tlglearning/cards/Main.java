e985eb3a924631c43394cb9b2929a6a6634cc24f
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-27 14:23:20 -0700

    Inline anonymous class in main method, previous push not inline

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -9,21 +9,6 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
-     Comparator<Card> comparator = new Comparator<>() {
-
-      @Override
-      public int compare(Card card1, Card card2) {
-        int comparison = card1.suit().color().compareTo(card2.suit().color());
-        if (comparison == 0) {
-          comparison = card1.suit().compareTo(card2.suit());
-          if (comparison == 0) {
-            comparison = - card1.rank().compareTo(card2.rank());
-          }
-
-        }
-        return comparison;
-      }
-    };
 
     Deck deck = new Deck();
     System.out.println(deck);
@@ -32,9 +17,20 @@ public class Main {
     System.out.println(deck);
     deck.sort();
     System.out.println(deck);
-    deck.sort(comparator);
+    deck.sort(new Comparator<>() {
+      @Override
+      public int compare(Card card1, Card card2) {
+        int comparison = card1.suit().color().compareTo(card2.suit().color());
+        if (comparison == 0) {
+          comparison = card1.suit().compareTo(card2.suit());
+          if (comparison == 0) {
+            comparison = - card1.rank().compareTo(card2.rank());
+          }
+        }
+        return comparison;
+      }
+    });
     System.out.println(deck);
-
   }
 
 }
